f2a212c40298d19ee1850623885fa17d28a0649b
Thibaut Caselli <thibaut.caselli@gmail.com>
2019-09-17 10:27:48 +0200

    Code cleanup

--- src/test/java/com/daikit/generics/utils/callandgetmethod/data/Class1.java
+++ src/test/java/com/daikit/generics/utils/callandgetmethod/data/Class1.java
@@ -21,75 +21,75 @@ import com.daikit.generics.utils.callandgetmethod.data.Results.ResultNotUsed;
 
 public class Class1 {
 
-	public Result1 test(String param1) {
+	public Result1 test(final String param1) {
 		return new Result1();
 	}
 
-	public Result2 test(int param1) {
+	public Result2 test(final int param1) {
 		return new Result2();
 	}
 
-	public ResultNotUsed test(String param1, String param2) {
+	public ResultNotUsed test(final String param1, final String param2) {
 		return new ResultNotUsed();
 	}
 
-	public Result3 test(int param1, String param2) {
+	public Result3 test(final int param1, final String param2) {
 		return new Result3();
 	}
 
-	public Result5 test(String param1, String param2, String... param3) {
+	public Result5 test(final String param1, final String param2, final String... param3) {
 		return new Result5();
 	}
 
-	public Result6 test(int param1, Object param2) {
+	public Result6 test(final int param1, final Object param2) {
 		return new Result6();
 	}
 
-	public Result7 test(InterfaceParam1 param2) {
+	public Result7 test(final InterfaceParam1 param2) {
 		return new Result7();
 	}
 
-	public Result8 test(InterfaceParam2 param2) {
+	public Result8 test(final InterfaceParam2 param2) {
 		return new Result8();
 	}
 
-	public Result9 test(InterfaceParam3 param3) {
+	public Result9 test(final InterfaceParam3 param3) {
 		return new Result9();
 	}
 
-	public Result10 test(ClassParam1 param2) {
+	public Result10 test(final ClassParam1 param2) {
 		return new Result10();
 	}
 
-	public Result11 test(ClassParam2 param2) {
+	public Result11 test(final ClassParam2 param2) {
 		return new Result11();
 	}
 
-	public Result12 test(ClassParam3 param3) {
+	public Result12 test(final ClassParam3 param3) {
 		return new Result12();
 	}
 
-	public Result14 test(String param1, String param2, Integer... param3) {
+	public Result14 test(final String param1, final String param2, final Integer... param3) {
 		return new Result14();
 	}
 
-	public Result15 test(String param1, String param2, Object... param3) {
+	public Result15 test(final String param1, final String param2, final Object... param3) {
 		return new Result15();
 	}
 
-	public Result16 test(String param1, String param2, InterfaceParam1... param3) {
+	public Result16 test(final String param1, final String param2, final InterfaceParam1... param3) {
 		return new Result16();
 	}
 
-	public Result17 test(String param1, String param2, InterfaceParam2... param3) {
+	public Result17 test(final String param1, final String param2, final InterfaceParam2... param3) {
 		return new Result17();
 	}
 
-	public Result18 test(String param1, String param2, ClassParam1... param3) {
+	public Result18 test(final String param1, final String param2, final ClassParam1... param3) {
 		return new Result18();
 	}
 
-	public Result19 test(String param1, String param2, ClassParam2... param3) {
+	public Result19 test(final String param1, final String param2, final ClassParam2... param3) {
 		return new Result19();
 	}
 }
--- src/test/java/com/daikit/generics/utils/callandgetmethod/data/Class2.java
+++ src/test/java/com/daikit/generics/utils/callandgetmethod/data/Class2.java
@@ -5,7 +5,7 @@ import com.daikit.generics.utils.callandgetmethod.data.Results.Result13;
 public class Class2 extends Class1 {
 
 	@Override
-	public Result13 test(String param1) {
+	public Result13 test(final String param1) {
 		return new Result13();
 	}
 
